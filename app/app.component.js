2ae3c3d898bd25c43bbf281daccc4731f3ca8aee
Jon J <jonjamison3@gmail.com>
2016-06-12 07:51:09 -0400

    adding basic array, so far so good

--- app/app.component.js
+++ app/app.component.js
@@ -13,21 +13,19 @@ var AppComponent = (function () {
     function AppComponent() {
     }
     AppComponent.prototype.changeFunction = function () {
-        console.log("here is a message including " + this.testResult);
     };
     AppComponent.prototype.ngOnInit = function () {
-        console.log("here is a message");
+        this.testResult = "";
+        this.testResults = new Array();
     };
     ;
     AppComponent.prototype._constructor = function () {
-        this.testResult = "";
-        return function () { return console.log("Let the games begin"); };
     };
     ;
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n  <h1>My... fourth(?) Angular 2 App</h1>\n  <select [(ngModel)]=\"testResult\">\n    <option>Option 1</option>\n    <option>Option 2</option>\n    <option>Option 3</option>\n    <option>Option 4</option>\n    <option>Option 5</option>\n    <option>Option 6</option>\n  </select>\n  <h2>{{testResult}}</h2>\n  ",
+            template: "\n  <div class=\"col-xs-6 col-xs-offset-3\">\n    <h1>My Angular2 App for Select/OnChange</h1>\n    <select [(ngModel)]=\"testResult\">\n      <option>Option 1</option>\n      <option>Option 2</option>\n      <option>Option 3</option>\n      <option>Option 4</option>\n      <option>Option 5</option>\n      <option>Option 6</option>\n    </select>\n\n    <h2 class=\"important\" [hidden]=\"testResult==''\">{{testResult}} has been selected</h2>\n  </div>\n  <div class=\"col-xs-6 col-xs-offset-3 well\">\n    <div class=\"col-xs-6 col-xs-offset-3\">\n      <input type=\"text\" [(ngModel)]=\"testResults[0]\" placeholder=\"This is a field linked to array value 1\"/>\n      <input type=\"text\" [(ngModel)]=\"testResults[1]\" placeholder=\"This is a field linked to array value 2\"/>\n      <input type=\"text\" [(ngModel)]=\"testResults[2]\" placeholder=\"This is a field linked to array value 3\"/>\n      <input type=\"text\" [(ngModel)]=\"testResults[3]\" placeholder=\"This is a field linked to array value 4\"/>\n      <input type=\"text\" [(ngModel)]=\"testResults[4]\" placeholder=\"This is a field linked to array value 5\"/>\n    </div>\n    <div class=\"clearfix\"></div>\n\n\n    <h3>Arrays working just fine</h3>\n    <ul class=\"list-group\">\n      <li class=\"list-group-item\">{{testResults[0]}}</li>\n      <li class=\"list-group-item\">{{testResults[1]}}</li>\n      <li class=\"list-group-item\">{{testResults[2]}}</li>\n      <li class=\"list-group-item\">{{testResults[3]}}</li>\n      <li class=\"list-group-item\">{{testResults[4]}}</li>\n    </ul>\n  </div>\n  <div class=\"col-xs-6 col-xs-offset-3\">\n  </div>\n  ",
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,32 +2,57 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>My Angular2 App for Select/OnChange</h1>
-  <select [(ngModel)]="testResult">
-    <option>Option 1</option>
-    <option>Option 2</option>
-    <option>Option 3</option>
-    <option>Option 4</option>
-    <option>Option 5</option>
-    <option>Option 6</option>
-  </select>
-  <h2>{{testResult}}</h2>
+  <div class="col-xs-6 col-xs-offset-3">
+    <h1>My Angular2 App for Select/OnChange</h1>
+    <select [(ngModel)]="testResult">
+      <option>Option 1</option>
+      <option>Option 2</option>
+      <option>Option 3</option>
+      <option>Option 4</option>
+      <option>Option 5</option>
+      <option>Option 6</option>
+    </select>
+
+    <h2 class="important" [hidden]="testResult==''">{{testResult}} has been selected</h2>
+  </div>
+  <div class="col-xs-6 col-xs-offset-3 well">
+    <div class="col-xs-6 col-xs-offset-3">
+      <input type="text" [(ngModel)]="testResults[0]" placeholder="This is a field linked to array value 1"/>
+      <input type="text" [(ngModel)]="testResults[1]" placeholder="This is a field linked to array value 2"/>
+      <input type="text" [(ngModel)]="testResults[2]" placeholder="This is a field linked to array value 3"/>
+      <input type="text" [(ngModel)]="testResults[3]" placeholder="This is a field linked to array value 4"/>
+      <input type="text" [(ngModel)]="testResults[4]" placeholder="This is a field linked to array value 5"/>
+    </div>
+    <div class="clearfix"></div>
+
+
+    <h3>Arrays working just fine</h3>
+    <ul class="list-group">
+      <li class="list-group-item">{{testResults[0]}}</li>
+      <li class="list-group-item">{{testResults[1]}}</li>
+      <li class="list-group-item">{{testResults[2]}}</li>
+      <li class="list-group-item">{{testResults[3]}}</li>
+      <li class="list-group-item">{{testResults[4]}}</li>
+    </ul>
+  </div>
+  <div class="col-xs-6 col-xs-offset-3">
+  </div>
   `,
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   testResult: String;
-
+  testResults: String[];
 
   changeFunction(){
-    console.log("here is a message including " + this.testResult);
+
   }
 
   ngOnInit(){
-    console.log("here is a message");
+    this.testResult = "";
+    this.testResults = new Array();
   };
 
   _constructor(){
-    this.testResult = "";
   };
 
 }
